3025e01a4eace40f62409137a2927a82c723b4fd
OakQ <oquinter@gmu.edu>
2016-02-17 00:18:27 -0500

    Paddle Dungeon
    Revenge of the Ghosts

--- js/main.js
+++ js/main.js
@@ -1,52 +1,57 @@
-window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/master/resources/Project%20Templates/Basic
+var game = new Phaser.Game(768, 768, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
+
+function preload() {
+    game.load.atlas('dungeonAtlas', 'assets/Sprites/dungeonAtlas.png', 'assets/Sprites/dungeonAtlas.json');
+}
+
+var spaces;
+var walls;
+var spookies;
+var booties;
+
+var space;
+var wall;
+var player;
+var spook;
+var booty;
+
+var place;
+
+function create() {
+    spaces = game.add.group();
+    walls = game.add.group();
+    for (var x = 0; x < 12; x++){
+        for (var y = 0; y < 12; y++){
+            if (x ==0 || x == 11 || y == 0 || y == 11)
+                wall = walls.create(x * 64, y * 64, 'dungeonAtlas', 'wall');
+            else
+                space = spaces.create(x * 64, y * 64, 'dungeonAtlas', 'ground');
+        }
+    }
+    //64+24, 64-24
+    player = game.add.sprite(88, 40, 'dungeonAtlas', 'paddle_01');
+    player.animations.add('slide', Phaser.Animation.generateFrameNames('paddle_', 0, 10, '', 2), 10, true);
+    player.animations.play('slide');
     
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
-    
-    "use strict";
-    
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
-    
-    function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+    spookies = game.add.group();
+    booties = game.add.group();
+    for (var x = 1; x < 11; x++){
+        for (var y = 1; y < 11; y++){
+            place = Math.floor(Math.random() * 25);
+            if(place < 2 && x != 1 && y != 1){
+                spook = spookies.create(x * 64, y * 64, 'dungeonAtlas', 'ghost_1');
+                spook.animations.add('rattle', Phaser.Animation.generateFrameNames('ghost_', 0, 2, '', 1), 2, true);
+                spook.animations.play('rattle');
+            }
+            else if(place >= 2 && place < 3 && x != 1 && y != 1){
+                booty = booties.create(x * 64, y * 64, 'dungeonAtlas', 'chest_1');
+                booty.animations.add('open', Phaser.Animation.generateFrameNames('chest_', 0, 3, '', 1), 1, false);
+            }
+        }
     }
     
-    var bouncy;
-    
-    function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
-        
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something awesome.", style );
-        text.anchor.setTo( 0.5, 0.0 );
-    }
-    
-    function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
-    }
-};
+}
+
+function update () {
+
+}
