1b85c964bec332434826765ddea881e8917921bd
OakQ <oquinter@gmu.edu>
2016-02-17 23:38:42 -0500

    V2
    gameplay is in

--- js/main.js
+++ js/main.js
@@ -2,56 +2,195 @@ var game = new Phaser.Game(768, 768, Phaser.AUTO, 'phaser-example', { preload: p
 
 function preload() {
     game.load.atlas('dungeonAtlas', 'assets/Sprites/dungeonAtlas.png', 'assets/Sprites/dungeonAtlas.json');
+    game.load.audio('hit_1', 'assets/Audio/Hit_1.wav');
+    game.load.audio('hit_2', 'assets/Audio/Hit_2.wav');
+    game.load.audio('hit_3', 'assets/Audio/Hit_3.wav');
+    game.load.audio('hit_4', 'assets/Audio/Hit_4.wav');
+    game.load.audio('hit_5', 'assets/Audio/Hit_5.wav');
+    game.load.audio('coins', 'assets/Audio/key_pickup.mp3');
+    game.load.audio('scream', 'assets/Audio/scream_horror1.mp3');
+    game.load.audio('music', 'assets/Audio/William_Hellfire_-_21_-_Poses_-_William_Hellfire.mp3');
 }
 
 var spaces;
 var walls;
 var spookies;
 var booties;
-
 var space;
 var wall;
 var player;
-var spook;
+var spooky;
 var booty;
-
 var place;
+var down;
+var up;
+var left;
+var right;
+var yourTurn;
+var text;
+var scoreText;
+var score;
+var gameOver;
 
 function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    
+    music = game.add.audio('music');
+    music.loop = true;
+    music.play();
+    
+    gameOver = false; //the game can't end before it starts
+    yourTurn = true; //start with the player's turn
     spaces = game.add.group();
-    walls = game.add.group();
+    walls = game.add.physicsGroup(); //doesn't work as intended
     for (var x = 0; x < 12; x++){
         for (var y = 0; y < 12; y++){
             if (x ==0 || x == 11 || y == 0 || y == 11)
-                wall = walls.create(x * 64, y * 64, 'dungeonAtlas', 'wall');
+                wall = walls.create(x * 64, y * 64, 'dungeonAtlas', 'wall'); //places walls on the 4 sides
             else
-                space = spaces.create(x * 64, y * 64, 'dungeonAtlas', 'ground');
+                space = spaces.create(x * 64, y * 64, 'dungeonAtlas', 'ground'); //spaces in every other space
         }
     }
-    //64+24, 64-24
-    player = game.add.sprite(88, 40, 'dungeonAtlas', 'paddle_01');
-    player.animations.add('slide', Phaser.Animation.generateFrameNames('paddle_', 0, 10, '', 2), 10, true);
+    player = game.add.sprite(88, 70, 'dungeonAtlas', 'paddle_01');
+    player.animations.add('slide', Phaser.Animation.generateFrameNames('paddle_', 1, 10, '', 2), 10, true); //create the animation of player
     player.animations.play('slide');
+    game.physics.enable(player);
     
     spookies = game.add.group();
+    spookies.enableBody = true;
+    
     booties = game.add.group();
     for (var x = 1; x < 11; x++){
         for (var y = 1; y < 11; y++){
             place = Math.floor(Math.random() * 25);
-            if(place < 2 && x != 1 && y != 1){
-                spook = spookies.create(x * 64, y * 64, 'dungeonAtlas', 'ghost_1');
-                spook.animations.add('rattle', Phaser.Animation.generateFrameNames('ghost_', 0, 2, '', 1), 2, true);
-                spook.animations.play('rattle');
+            if(place < 2 && x != 1 && y != 1){ //2/25 chance of placing down a ghost
+                spooky = spookies.create(x * 64, y * 64, 'dungeonAtlas', 'ghost_1');
+                spooky.animations.add('rattle', Phaser.Animation.generateFrameNames('ghost_', 1, 2, '', 1), 2, true);
+                spooky.animations.play('rattle');
+                spooky.body.collideWorldBounds = true; //try to keep them inside. Doesn't work as intended
             }
-            else if(place >= 2 && place < 3 && x != 1 && y != 1){
+            else if(place >= 2 && place < 3 && x != 1 && y != 1){ // 1/25 chance of putting down a chest
                 booty = booties.create(x * 64, y * 64, 'dungeonAtlas', 'chest_1');
-                booty.animations.add('open', Phaser.Animation.generateFrameNames('chest_', 0, 3, '', 1), 1, false);
+                booty.animations.add('open', Phaser.Animation.generateFrameNames('chest_', 1, 3, '', 1), 1, false);
+                booty.animations.killOnComplete = true; //deletes chest when aniamtion finishes. Doesn't work as intended
             }
         }
     }
-    
+    down = game.input.keyboard.addKey(Phaser.Keyboard.S); //use WASD instead of arrow keys because we are not casuals
+    up = game.input.keyboard.addKey(Phaser.Keyboard.W);
+    left = game.input.keyboard.addKey(Phaser.Keyboard.A);
+    right = game.input.keyboard.addKey(Phaser.Keyboard.D);
+    score = 0;
+    scoreText = game.add.text(32, 32, 'Score: 0', { font: "20px Arial", fill: "#000000", align: "center" }); //set score to 0 and set text
 }
 
-function update () {
 
+function update () {
+    //call on the different move functions when we hit a button. onDown makes it so it only works once per press
+    down.onDown.add(moveDown);
+    up.onDown.add(moveUp);
+    left.onDown.add(moveLeft);
+    right.onDown.add(moveRight);
+
+    if (!yourTurn) //once our turn ends, the enmy takes a turn
+        enemyTurn();
+    game.physics.arcade.collide(player, walls); //doesn't work
+    hit(); //checks for collsions
+}
+
+
+function moveDown(){    
+    if(yourTurn && !gameOver){ //only works if it is the player's turn and the game isn't over
+        game.add.tween(player).to( { x: player.world.x, y: player.world.y + 64 }, 1000, "Linear", true); //move the player relative to its location slowly
+        yourTurn = false; //end turn
+    }
+}
+
+function moveUp(){
+    if(yourTurn && !gameOver){
+        game.add.tween(player).to( { x: player.world.x, y: player.world.y - 64 }, 1000, "Linear", true);
+        yourTurn = false;
+    }
+}
+
+function moveLeft(){
+    if(yourTurn && !gameOver){
+        game.add.tween(player).to( { x: player.world.x - 64, y: player.world.y }, 1000, "Linear", true);
+        yourTurn = false;
+    }
+}
+
+function moveRight(){
+    if(yourTurn && !gameOver){
+        game.add.tween(player).to( { x: player.world.x + 64, y: player.world.y }, 1000, "Linear", true);
+        yourTurn = false;
+    }    
+}
+
+var move;
+var enemyTween;
+function enemyTurn(){ //the enemy moves randomly for its turn
+    for (var i = 0; i < spookies.countLiving(); i++){ //go through and move each ghost
+        spooky = spookies.getChildAt(i); //using each ghost
+        move = Math.floor(Math.random() * 4);
+        if (move == 0) //move ghosts according to rng
+            enemyTween = game.add.tween(spooky).to( { x: spooky.world.x, y: spooky.world.y + 64 }, 1000, "Linear", true);
+        if (move == 1)
+            enemyTween = game.add.tween(spooky).to( { x: spooky.world.x, y: spooky.world.y - 64 }, 1000, "Linear", true);
+        if (move == 2)
+            enemyTween = game.add.tween(spooky).to( { x: spooky.world.x + 64, y: spooky.world.y }, 1000, "Linear", true);
+        if (move == 3)
+            enemyTween = game.add.tween(spooky).to( { x: spooky.world.x - 64, y: spooky.world.y }, 1000, "Linear", true);
+    }
+    enemyTween.onComplete.add(playersTurn); //player can't move until ghosts are done
+}
+
+function playersTurn(){
+    yourTurn = true; //player's turn
+}
+
+var col;
+var scream;
+var rand; 
+function hit(){ //checks for overlapping collisions
+    for(var i = 0; i < booties.length; i++){ //for each treasure
+        if(checkOverlap(player, booties.getChildAt(i)) && booties.getChildAt(i).alive){ //if we overlap a live one
+            score += 1000;
+            scoreText.text = 'Score: ' + score; //update score
+            booties.getChildAt(i).animations.play('open'); //play animation. Technically doesn't work due to kill command
+            booties.getChildAt(i).kill(); //kill chest
+            col = game.add.audio('coins');
+            col.play();
+            checkGameOver(); //when we open a chest, we check to see if there any left
+        }
+    }  
+    for(var i = 0; i < spookies.length; i++){
+        if(checkOverlap(player, spookies.getChildAt(i)) && spookies.getChildAt(i).alive){
+            score -= 200; //lose point for getting spooked
+            scoreText.text = 'Score: ' + score;
+            spookies.getChildAt(i).kill(); //kill the spooky
+            rand = Math.floor(Math.random() * 5) + 1;//number from 1 to 5
+            col = game.add.audio('hit_' + rand); //choose repsective hit sound
+            col.play(); //play sound
+            scream = game.add.audio('scream');
+            scream.allowMultiple = true; //extra spooky
+            scream.play(); //i m so spooped rite nao
+        }
+    }
+}
+
+function checkOverlap(spriteA, spriteB) { //check for overlaps between player and objects
+        
+    var boundsA = spriteA.getBounds(); //get the bounds of their collider
+    var boundsB = spriteB.getBounds();
+
+    return Phaser.Rectangle.intersects(boundsA, boundsB); //see if they intersect
+
+}
+
+function checkGameOver(){
+    if(booties.countLiving() == 0){ //if no chests remain
+        scoreText.text = "THE PADDLE CLAN IS VICTORIOUS! Score: " + score; //display win text
+        gameOver = true; //end the game
+    }
 }
