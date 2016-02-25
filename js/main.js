3b85a6979a4e55790d996fb558e7fc77cbcb1071
Oscar Quinteros <oquinter@gmu.edu>
2016-02-25 11:26:19 -0500

    wheel fix

--- js/main.js
+++ js/main.js
@@ -96,10 +96,10 @@ function create() {
     wall4 = walls.create(608, 80, 'spookyAtlas', 'wall');
     walls.setAll('body.immovable', true);
 
-    wheel1 = walls.create(160, 109, 'spookyAtlas', 'wheel_1');
-    wheel2 = walls.create(256, 109, 'spookyAtlas', 'wheel_1');
-    wheel3 = walls.create(352, 109, 'spookyAtlas', 'wheel_1');
-    wheel4 = walls.create(448, 109, 'spookyAtlas', 'wheel_1');
+    wheel1 = wheels.create(160, 109, 'spookyAtlas', 'wheel_1');
+    wheel2 = wheels.create(256, 109, 'spookyAtlas', 'wheel_1');
+    wheel3 = wheels.create(352, 109, 'spookyAtlas', 'wheel_1');
+    wheel4 = wheels.create(448, 109, 'spookyAtlas', 'wheel_1');
     wheels.callAll('animations.add', 'animations', 'turn', Phaser.Animation.generateFrameNames('wheel_', 1, 3, '', 1), 3, false);
 
     reset = game.add.sprite(64, 109, 'spookyAtlas', 'reset_1');
